e20bd5ab7a69337bd5db22db615f03188188c652
kbini <kbini@xpanxion.com>
2021-09-02 12:35:33 -0400

    added description field to IProduct model/interface completed project.service file with getProjects and getProject (by id)

--- src/app/projects/project.ts
+++ src/app/projects/project.ts
@@ -2,7 +2,9 @@
 // can add an image or star rating component
 
 export interface IProject {
+  projectId: number;
   projectName: string;
+  description: string; // field added to java model 9/2/21
   difficulty: string;
   category: string;
   priority: string;
