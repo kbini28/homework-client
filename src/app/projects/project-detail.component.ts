1a54a0690c0b3c4104ae7da43f7ff9b6e167c96c
kbini <kbini@xpanxion.com>
2021-09-02 15:44:11 -0400

    modified project-list and project-detail components in ts and html have not tested in localhost yet, committing changes then merging to main branch still need to create an add component form

--- src/app/projects/project-detail.component.ts
+++ src/app/projects/project-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProject } from './project';
+import { ProjectService } from './project.service';
 
 @Component({
   selector: 'pm-project-detail',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectDetailComponent implements OnInit {
 
-  constructor() { }
+  pageTitle = 'Project Details';
+  errorMessage = '';
+  project: IProject | undefined;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private projectService: ProjectService) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.getProject(id);
+    }
+  }
+
+  getProject(id: number): void {
+    this.projectService.getProject(id).subscribe({
+      next: project => this.project = project,
+      error: err => this.errorMessage = err
+    });
+  }
+
+  onBack(): void {
+    this.router.navigate(['/projects']);
   }
 
 }
--- src/app/projects/project-list.component.ts
+++ src/app/projects/project-list.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IProject } from './project';
+import { ProjectService } from './project.service';
 
 @Component({
   selector: 'pm-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.css']
 })
-export class ProjectListComponent implements OnInit {
+export class ProjectListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  pageTitle = 'Project List';
+  // imageWidth = 50;
+  // imageMargin = 2;
+  // showImage = false;
+  errorMessage = '';
+  sub!: Subscription;
 
-  ngOnInit(): void {
+  private _listFilter = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProjects = this.performFilter(value);
   }
 
+  filteredProjects: IProject[] = [];
+  projects: IProject[] = [];
+
+  constructor(private projectService: ProjectService) { }
+
+  performFilter(filterBy: string): IProject[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.projects.filter((project: IProject) =>
+      project.projectName.toLocaleLowerCase().includes(filterBy));
+  }
+
+  // site does not have images (or anything to toggle) yet
+  // toggleImage(): void {
+  //   this.showImage = !this.showImage;
+  // }
+
+  ngOnInit(): void {
+    this.sub = this.projectService.getProjects().subscribe({
+      next: projects => {
+        this.projects = projects;
+        this.filteredProjects = this.projects;
+      },
+      error: err => this.errorMessage = err
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  // site does not have anything to do with ratings yet...
+  // onRatingClicked(message: string): void {
+  //   this.pageTitle = 'Product List: ' + message;
+  // }
 }
